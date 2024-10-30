d8c6471ea385c161dd5223f9f24776312a75687e
Veh-ware <developer@veh-ware.com>
2024-10-30 12:50:59 -0700

    Update PaymentPage.jsx

--- src/component/PaymentPage.jsx
+++ src/component/PaymentPage.jsx
@@ -363,7 +363,7 @@ const PaymentPage = () => {
    useEffect(() => {
         const fetchPaymentDetails = async () => {
             try {
-                const response = await fetch(`https://vehware-invoice-backend.vercel.app/api/get-payment-details/${sessionId}`);
+                const response = await fetch(`https://stripe-backend-sand.vercel.app/api/get-payment-details/${sessionId}`);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
@@ -452,7 +452,7 @@ const PaymentPage = () => {
         } else if (paymentIntent && paymentIntent.status === 'succeeded') {
             try {
                 console.log("paymentIntent=>", paymentIntent.status)
-                const response = await fetch(`https://vehware-invoice-backend.vercel.app/api/create-payment/${sessionId}`, {
+                const response = await fetch(`https://stripe-backend-sand.vercel.app/api/create-payment/${sessionId}`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
